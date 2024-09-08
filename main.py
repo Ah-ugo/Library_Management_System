d0b4e1913d498e7baaa39ab823b6a8b2fc5f0c46
Ah-ugo <ahuekweprinceugo@gmail.com>
2024-09-08 17:16:42 +0100

    update

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ from pydantic.functional_validators import BeforeValidator
 import shutil
 from typing_extensions import Annotated
 from fastapi.staticfiles import StaticFiles
+from pymongo import MongoClient
 
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://parabellum:bluu12345@cluster0.5kumd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+client = MongoClient('mongodb+srv://parabellum:bluu12345@cluster0.5kumd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 db = client.LMS
 
@@ -90,35 +91,35 @@ app.mount("/uploaded_books_images", StaticFiles(directory="uploaded_books_images
 
 # Manage Students
 @app.get("/students", tags=["Students"])
-async def get_Students():
+def get_Students():
     mapArr = []
     all_students = student_collection.find({})
 
-    async for student in all_students:
+    for student in all_students:
         mapArr.append(Student(**student))
 
     return mapArr
 
 @app.post("/students", tags=["Students"])
-async def add_Student(student:Student):
-    addStudent = await student_collection.insert_one(student.dict())
-    result = await student_collection.find_one({"_id":addStudent.inserted_id})
+def add_Student(student:Student):
+    addStudent = student_collection.insert_one(student.dict())
+    result = student_collection.find_one({"_id":addStudent.inserted_id})
 
     result["_id"] = str(result["_id"])
 
     return result
 
 @app.get("/students/{id}" , tags=["Students"])
-async def getStudentById(id:str):
-    student = await student_collection.find_one({"_id": ObjectId(id)})
+def getStudentById(id:str):
+    student = student_collection.find_one({"_id": ObjectId(id)})
 
     student["_id"] = str(student["_id"])
 
     return student
 
 @app.put("/students/{id}", tags=["Students"])
-async def editStudent(id:str, student: Student):
-    student = await student_collection.find_one_and_update({"_id": ObjectId(id)},
+def editStudent(id:str, student: Student):
+    student = student_collection.find_one_and_update({"_id": ObjectId(id)},
             {"$set": student.dict()},
             return_document=True)
     student["_id"] = str(student["_id"])
@@ -126,30 +127,30 @@ async def editStudent(id:str, student: Student):
     return student
 
 @app.delete("/students/{id}", tags=["Students"])
-async def deleteStudent(id:str):
-    await student_collection.delete_one({"_id": ObjectId(id)})
+def deleteStudent(id:str):
+    student_collection.delete_one({"_id": ObjectId(id)})
     students = []
     getStudents = student_collection.find({})
 
-    async for student in getStudents:
+    for student in getStudents:
         students.append(Student(**student))
 
     return students
 
 # Manage Books
 @app.get("/books", tags=["Books"])
-async def get_Books():
+def get_Books():
     mapArr = []
     all_books = book_collection.find({})
 
-    async for book in all_books:
+    for book in all_books:
         mapArr.append(Book(**book))
 
     return mapArr
 
 # Important for future projects that involve uploading images!!!!!!!!!!!
 @app.post("/books", tags=["Books"])
-async def add_Book(
+def add_Book(
 request: Request,
         title: str = Form(...),
         author: str = Form(...),
@@ -170,24 +171,24 @@ request: Request,
         book.image_url = image_url  # Assign image URL to the book
 
     # Insert the book into MongoDB
-    addBook = await book_collection.insert_one(book.dict())
-    result = await book_collection.find_one({"_id": addBook.inserted_id})
+    addBook = book_collection.insert_one(book.dict())
+    result = book_collection.find_one({"_id": addBook.inserted_id})
     result["_id"] = str(result["_id"])
 
     return result
 
 
 @app.get("/books/{id}" , tags=["Books"])
-async def getBookById(id:str):
-    book = await book_collection.find_one({"_id": ObjectId(id)})
+def getBookById(id:str):
+    book = book_collection.find_one({"_id": ObjectId(id)})
 
     book["_id"] = str(book["_id"])
 
     return book
 
 @app.put("/books/{id}", tags=["Books"])
-async def editBook(id:str, book: Book):
-    booky = await book_collection.find_one_and_update({"_id": ObjectId(id)},
+def editBook(id:str, book: Book):
+    booky = book_collection.find_one_and_update({"_id": ObjectId(id)},
             {"$set": book.dict()},
             return_document=True)
     booky["_id"] = str(booky["_id"])
@@ -195,12 +196,12 @@ async def editBook(id:str, book: Book):
     return booky
 
 @app.delete("/books/{id}", tags=["Books"])
-async def deleteBook(id:str):
-    await book_collection.delete_one({"_id": ObjectId(id)})
+def deleteBook(id:str):
+    book_collection.delete_one({"_id": ObjectId(id)})
     books = []
     getBooks = book_collection.find({})
 
-    async for book in getBooks:
+    for book in getBooks:
         books.append(Book(**book))
 
     return books
@@ -208,30 +209,30 @@ async def deleteBook(id:str):
 # User Endpoint
 
 @app.get("/users", tags=["Users"])
-async def get_all_users():
+def get_all_users():
     all_Users = user_collection.find({})
 
     user_Arr = []
 
-    async for user in all_Users:
+    for user in all_Users:
         user_Arr.append(User(**user))
 
     return user_Arr
 
 @app.post("/users", tags=["Users"])
-async def add_user(user:User):
-    addUser = await user_collection.insert_one(user.dict())
-    result = await user_collection.find_one({"_id": addUser.inserted_id})
+def add_user(user:User):
+    addUser = user_collection.insert_one(user.dict())
+    result = user_collection.find_one({"_id": addUser.inserted_id})
 
     result["_id"] = str(result["_id"])
     return result
 
 
 @app.get("/users/login", tags=["Users"])
-async def Login_User(email: str, password: str):
+def Login_User(email: str, password: str):
     try:
         # Find user by email and password
-        query_User = await user_collection.find_one({"email": email, "password": password})
+        query_User = user_collection.find_one({"email": email, "password": password})
 
         # Check if the user exists
         if query_User:
@@ -246,8 +247,8 @@ async def Login_User(email: str, password: str):
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
 
 @app.get("/users/{id}", tags=["Users"])
-async def get_User_By_Id(id:str):
-    getUser = await user_collection.find_one({"_id": ObjectId(id)})
+def get_User_By_Id(id:str):
+    getUser = user_collection.find_one({"_id": ObjectId(id)})
 
     getUser["_id"] = str(getUser["_id"])
 
@@ -255,9 +256,9 @@ async def get_User_By_Id(id:str):
 
 
 @app.put("/users/{id}", tags=["Users"])
-async def Edit_User_Data(id: str, user_update: User):
+def Edit_User_Data(id: str, user_update: User):
     # Fetch the existing user data
-    existing_user = await user_collection.find_one({"_id": ObjectId(id)})
+    existing_user = user_collection.find_one({"_id": ObjectId(id)})
     if not existing_user:
         raise HTTPException(status_code=404, detail="User not found")
 
@@ -266,7 +267,7 @@ async def Edit_User_Data(id: str, user_update: User):
 
     # Update only the fields provided
     if update_data:
-        updated_user = await user_collection.find_one_and_update(
+        updated_user = user_collection.find_one_and_update(
             {"_id": ObjectId(id)},
             {"$set": update_data},
             return_document=True
@@ -282,12 +283,12 @@ async def Edit_User_Data(id: str, user_update: User):
 
 
 @app.delete("/users/{id}", tags=["Users"])
-async def Delete_User(id:str):
-    await user_collection.delete_one({"_id": ObjectId(id)})
+def Delete_User(id:str):
+    user_collection.delete_one({"_id": ObjectId(id)})
     users = []
     getUsers = user_collection.find({})
 
-    async for user in getUsers:
+    for user in getUsers:
         users.append(User(**user))
 
     return users
@@ -295,37 +296,37 @@ async def Delete_User(id:str):
 # Borrowing
 
 @app.get("/borrowing", tags=["Borrowings"])
-async def Get_All_Borrowings():
+def Get_All_Borrowings():
     allBorrowings = borrowing_collection.find({})
 
     borrowings = []
 
-    async for borrows in allBorrowings:
+    for borrows in allBorrowings:
         borrowings.append(Borrowing(**borrows))
 
     return borrowings
 
 @app.post("/borrowing",tags=["Borrowings"])
-async def Add_Borrowing(body:Borrowing):
-    addToDB = await borrowing_collection.insert_one(body.dict())
+def Add_Borrowing(body:Borrowing):
+    addToDB = borrowing_collection.insert_one(body.dict())
 
-    viewAdded = await borrowing_collection.find_one({"_id": ObjectId(addToDB.inserted_id)})
+    viewAdded = borrowing_collection.find_one({"_id": ObjectId(addToDB.inserted_id)})
 
     viewAdded["_id"] = str(viewAdded["_id"])
 
     return viewAdded
 
 @app.get("/borrowing/{id}", tags=["Borrowings"])
-async def Get_Borrowing_By_Id(id:str):
-    getBorrowing = await borrowing_collection.find_one({"_id": ObjectId(id)})
+def Get_Borrowing_By_Id(id:str):
+    getBorrowing = borrowing_collection.find_one({"_id": ObjectId(id)})
 
     getBorrowing["_id"] = str(getBorrowing["_id"])
 
     return getBorrowing
 
 @app.put("/borrowing/{id}", tags=["Borrowings"])
-async def Update_Borrowing(id:str, body:Borrowing):
-    updateItem = await borrowing_collection.find_one_and_update(
+def Update_Borrowing(id:str, body:Borrowing):
+    updateItem = borrowing_collection.find_one_and_update(
             {"_id": ObjectId(id)},
             {"$set": body.dict()},
             return_document=True
@@ -336,14 +337,14 @@ async def Update_Borrowing(id:str, body:Borrowing):
     return updateItem
 
 @app.delete("/borrowing/{id}", tags=["Borrowings"])
-async def Delete_Borrowing(id:str):
-    await borrowing_collection.delete_one({"_id": ObjectId(id)})
+def Delete_Borrowing(id:str):
+    borrowing_collection.delete_one({"_id": ObjectId(id)})
 
     getAllItems = borrowing_collection.find({})
 
     allItems = []
 
-    async for item in getAllItems:
+    for item in getAllItems:
         allItems.append(Borrowing(**item))
 
     return allItems
@@ -351,19 +352,19 @@ async def Delete_Borrowing(id:str):
 # Requests
 
 @app.get("/requests", tags=["Requests"])
-async def Get_All_Requests():
+def Get_All_Requests():
     getRequests = request_collection.find({})
     requests = []
 
-    async for request in getRequests:
+    for request in getRequests:
         requests.append(BorrowRequest(**request))
 
     return requests
 
 @app.post("/requests", tags=["Requests"])
-async def Create_Request(body:BorrowRequest):
-    createRequest = await request_collection.insert_one(body.dict())
-    getAddedReq = await request_collection.find_one({"_id": ObjectId(createRequest.inserted_id)})
+def Create_Request(body:BorrowRequest):
+    createRequest = request_collection.insert_one(body.dict())
+    getAddedReq = request_collection.find_one({"_id": ObjectId(createRequest.inserted_id)})
 
     getAddedReq["_id"] = str(getAddedReq["_id"])
 
@@ -372,5 +373,5 @@ async def Create_Request(body:BorrowRequest):
 
 
 @app.get("/")
-async def main ():
+def main ():
     return {"welcome to Ahuekwe Prince Ugochukwu first fastAPI Project"}
